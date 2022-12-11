ce231c3decbf2880f6b1ee007bfbbf9f26f0d200
Jakub Bilski <jakub@blsk.pl>
2022-12-11 14:19:58 +0100

    Added session handling.

--- src/index.php
+++ src/index.php
@@ -4,10 +4,14 @@ require_once './vendor/autoload.php';
 
 use Apsl\Http\Request;
 use Apsl\Http\Response;
+use Apsl\Session\Session;
 
 
 $request = new Request();
 $name = $request->getQueryStringValue(name: 'name', default: 'Anonymous');
+
+$session = new Session();
+$session->setValue('test', 'This is the test!');
 
 $response = new Response();
 $response->setBody("Hello {$name}!");
